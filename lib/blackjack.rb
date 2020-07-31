36693c819dedc12b90076150090e1cafb7bdb268
Benjamin Arthur Looper <benlooper08@gmail.com>
2020-07-31 16:01:53 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,37 +1,55 @@
+require 'pry'
+
 def welcome
-  # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
-  # code #deal_card here
+  Random.rand(1...12)
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(total)
+  puts "Your cards add up to #{total}"
 end
 
 def prompt_user
-  # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  # code #get_user_input here
+  gets.chomp
 end
 
-def end_game
-  # code #end_game here
+def end_game(total)
+  puts "Sorry, you hit #{total}. Thanks for playing!"
+  
 end
 
 def initial_round
-  # code #initial_round here
+  first_card = deal_card
+  second_card = deal_card
+  puts "Your cards add up to " + (first_card+second_card).to_s 
+  first_card + second_card
 end
 
-def hit?
-  # code hit? here
+def hit?(current_total)
+  prompt_user
+  action = get_user_input.downcase
+  if action == 's'
+    return current_total
+    return stay = true 
+  elsif action == 'h'
+    current_total = current_total + deal_card
+    return current_total
+  else 
+    invalid_command
+    prompt_user
+    action = get_user_input.downcase
+  end 
 end
 
 def invalid_command
-  # code invalid_command here
+  puts "Please enter a valid command"
 end
 
 #####################################################
@@ -39,6 +57,14 @@ end
 #####################################################
 
 def runner
-  # code runner here
+  welcome 
+  current_total = initial_round
+  stay = false 
+  until current_total > 21 or stay == true 
+    hit?(current_total)
+    stay = true 
+    display_card_total(current_total)
+  end 
+end_game(current_total)
 end
     
